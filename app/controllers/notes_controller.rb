6c2ea4878edb8a8d448f44f900920c37b03ac043
sundars0 <sundars04@rediffmail.com>
2016-11-05 00:36:10 +0000

    including other files to git

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,24 @@
 class NotesController < ApplicationController
-  def index    
+  before_action :find_note, :only => [:show, :edit, :update, :destroy]
+  def index
+    @notes = Note.all.order("created_at DESC")
   end
 
-  def show    
+  def show
+
   end
 
-  def new    
+  def new
+    @note = Note.new
   end
 
-  def create    
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render ('new')
+    end
   end
 
   def edit
@@ -22,9 +32,11 @@ class NotesController < ApplicationController
 
   private
 
-    def find_note      
+    def find_note
+      @note = Note.find(params[:id])
     end
 
     def note_params
+      params.require(:note).permit(:title, :content)
     end
 end
